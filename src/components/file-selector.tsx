d7026e1c21356ce8e83a691f52ec9a044c45f865
CoreyCole <coreyleoc@gmail.com>
2022-05-19 18:28:12 -0700

    fix(src): multi-file upload

--- src/components/file-selector.tsx
+++ src/components/file-selector.tsx
@@ -25,32 +25,27 @@ export class FileSelector extends React.Component<IFileSelectorProps, IFileSelec
 
   async upload(selectorFiles: FileList, index: number) {
     const thisTemp = this;
-    if (index > selectorFiles.length) {
-      console.error("selectorFiles overrun!");
+    if (index >= selectorFiles.length) {
       return;
     }
     const file = selectorFiles[index];
+    console.log(`uploading file: ${file.name}`);
     const data = new FormData();
     data.append("myFile", file);
     fetch("http://localhost:8080/upload", {
       mode: 'no-cors',
       method: "POST",
       body: data
-    }).then(function (res) {
-      if (res.ok) {
-        alert("Perfect! ");
-        thisTemp.upload(selectorFiles, index+1);
-      } else if (res.status === 401) {
-        alert("Oops! ");
-      }
-    }, function (e) {
-      alert("Error submitting form!");
+    }).then(function (_) {
+      thisTemp.upload(selectorFiles, index+1);
+    }, function (e: Error) {
+      alert(`Error submitting form! ${e.message}`);
     });
   }
 
   render() {
     return <>{<div>
-      <input type="file" name="myFile" onChange={(e) => this.handleChange(e.target.files)} />
+      <input type="file" name="myFile" multiple onChange={(e) => this.handleChange(e.target.files)} />
     </div>}</>;
   }
 }
